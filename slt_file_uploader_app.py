a13fd9bf094428845f289fd5214a8f815811182d
Jephte Dufermeau <dufabraham@gmail.com>
2026-01-16 17:42:41 +0100

    ajout d'une fonction de chargement de fichier

--- slt_file_uploader_app.py
+++ slt_file_uploader_app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pandas as pd
 
@@ -10,7 +11,29 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.subheader('DataFrame')
   st.write(df)
-  st.subheader('Descriptive Statistics')
-  st.write(df.describe())
+  num_of_rows = len(df)
+
+  fdf = df[df.Sex =="female"]
+  num_female = len(fdf)
+
+  mdf = df[df.Sex =="male"]
+  num_male = len(mdf)
+
+  st.info(f"How many people in total servived the calamity?: {num_of_rows}")
+
+  st.info(f'How many were females?:{num_female}')
+  st.write(fdf)
+
+  st.info(f'How many erew males ?:{num_male}')
+  st.write(mdf)
+
+  st.info('Showing total male vs female ratio:')
+
+  data = {"survived":[num_female,num_male], "gender":["female", "male"]}
+  df = pd.DataFrame(data)
+  df
+  st.bar_chart(data=df, x="gender", y="survived")
+
+
 else:
-  st.info('☝️ Upload a CSV file')
+    st.info('☝️ Upload a CSV file')
